bef0b33ea9cedb47803bde69892e75c903e061a4
Soumadeep Ghosh <144361361+itzsoumadeep@users.noreply.github.com>
2024-08-23 12:45:37 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
-const path = require('path');
 
 app.use(cors());
 app.use(express.json());
@@ -23,36 +22,19 @@ mongoose
     console.log(err.message);
   });
 
-
+app.get("/ping", (_req, res) => {
+  return res.json({ msg: "Ping Successful" });
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-////////////////////////////Diployment Code /////////////////////////////
-
-
-const __dirname1 = path.resolve();
-if(process.env.Node_ENV  === "production"){
-  app.use(express.static(path.join(__dirname1, "/public/build")));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname1, 'public', 'build', 'index.html'));
-  })
-}else{
-  app.get("/ping", (_req, res) => {
-    return res.json({ msg: "Ping Successful" });
-  });
-}
-
-
-////////////////////////////Diployment Code /////////////////////////////
 
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://client-swart-phi.vercel.app/",
     credentials: true,
   },
 });
